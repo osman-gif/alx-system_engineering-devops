60eeea12d3f3e1f71d7ffd935f62e8c48dd38bab
osman-gif <77807296+osman-gif@users.noreply.github.com>
2024-05-06 20:55:09 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Using Puppet, create a manifest that kills a process named killmenow.
-
-exec { 'killmenow':
-  command => 'pkill killmenow',
-  path    => '/usr/bin:/usr/sbin:/bin',
-}
--- 0x0A-configuration_management/README.md
+++
@@ -1,1 +0,0 @@
-This is ALX-SE program, 0x0A-configuration_managementp project
