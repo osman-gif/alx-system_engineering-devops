ffd0932c75f89ed434d928b14ba1c278dd2bb8e3
osman-gif <osmanngong@gmail.com>
2024-07-01 12:30:26 +0200

    Added the 100-puppet_ssh_config.pp file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+file_line{ 'config':
+  path  => '/home/ajang/config',
+  line  => "\tPasswordAuthentication no\n\tIdentityFile ~/.ssh/school",
+  match => '^\tPasswordAuthentication no\n\tIdentityFile ~/.ssh/school$',
+  append_on_no_match => false,
+}
+
