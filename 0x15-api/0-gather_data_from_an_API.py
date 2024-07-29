f794c0a114798d6f4c920f9a5fcddc01b24573bd
osman-gif <osmanngong@gmail.com>
2024-07-29 12:59:57 +0200

    Documented 0-gather_data_from_an_API.py file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,28 +9,28 @@ import requests
 import json
 from sys import argv
 
-emp_id = argv[1]
-todo_url = f'https://jsonplaceholder.typicode.com/todos/'
-user_url = f'https://jsonplaceholder.typicode.com/users/{emp_id}'
+if __name__ == '__main__':
+    emp_id = argv[1]
+    todo_url = f'https://jsonplaceholder.typicode.com/todos/'
+    user_url = f'https://jsonplaceholder.typicode.com/users/{emp_id}'
 
-user = requests.get(user_url)
-todos = requests.get(todo_url)
-completed_tasks = 0
-total_tasks = 0
-tasks_title_list = []
+    user = requests.get(user_url)
+    todos = requests.get(todo_url)
+    c_tasks = 0
+    t_tasks = 0
+    tasks_title_list = []
 
-emp_name = user.json().get('name')
-for index, todo in enumerate(todos.json()):
+    e_name = user.json().get('name')
+    for index, todo in enumerate(todos.json()):
 
-    if todo.get('userId') == int(emp_id):
-        total_tasks = total_tasks + 1
+        if todo.get('userId') == int(emp_id):
+            t_tasks = t_tasks + 1
 
-        if todo.get('completed'):
-            tasks_title_list.append(todo.get('title'))
-            completed_tasks = completed_tasks + 1
+            if todo.get('completed'):
+                tasks_title_list.append(todo.get('title'))
+                c_tasks = c_tasks + 1
 
-first_line = f"Employee {emp_name} is done with \
-tasks({completed_tasks}/{total_tasks})"
-print(first_line)
-for task_title in tasks_title_list:
-    print(f"     {task_title}")
+    first_line = f"Employee {e_name} is done with tasks({c_tasks}/{t_tasks})"
+    print(first_line)
+    for task_title in tasks_title_list:
+        print(f"     {task_title}")
